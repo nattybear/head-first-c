7946e251937e427b7affc3517b0f2beb5c846209
준규 <vpark45@gmail.com>
2021-12-30 21:30:38 +0900

    Add comments

--- chapter5/badunion.c
+++ chapter5/badunion.c
@@ -7,7 +7,13 @@ typedef union {
 
 int main()
 {
+  // By mistake, the
+  // programmer has set the
+  // weight, not the count
   cupcake order = {2};
+
+  // She set the weight, but
+  // she's reading the count
   printf("Cupcakes quantity: %i\n", order.count);
   return 0;
 }
