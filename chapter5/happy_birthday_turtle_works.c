e945bf8ff126d5fc9c773ae9e592b0f65a3e2a3f
준규 <vpark45@gmail.com>
2021-12-30 18:52:57 +0900

    Modify to use pointer

--- chapter5/happy_birthday_turtle_works.c
+++ chapter5/happy_birthday_turtle_works.c
@@ -6,11 +6,11 @@ typedef struct {
   int age;
 } turtle;
 
-void happy_birthday(turtle t)
+void happy_birthday(turtle *t)
 {
-  t.age = t.age + 1;
+  (*t).age = (*)t.age + 1;
   printf("Happy Birthday %s! You are now %i years old!\n",
-    t.name, t.age);
+    (*)t.name, (*)t.age);
 }
 
 int main()
