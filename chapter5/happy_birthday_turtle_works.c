36b1ec9613daf738c03b58b7c77d9e538a5e00a0
준규 <vpark45@gmail.com>
2021-12-30 18:58:55 +0900

    Fix typo and add missing operator &

--- chapter5/happy_birthday_turtle_works.c
+++ chapter5/happy_birthday_turtle_works.c
@@ -8,15 +8,15 @@ typedef struct {
 
 void happy_birthday(turtle *t)
 {
-  (*t).age = (*)t.age + 1;
+  (*t).age = (*t).age + 1;
   printf("Happy Birthday %s! You are now %i years old!\n",
-    (*)t.name, (*)t.age);
+    (*t).name, (*t).age);
 }
 
 int main()
 {
   turtle myrtle = {"Myrtle", "Leatherback sa turtle", 99};
-  happy_birthday(myrtle);
+  happy_birthday(&myrtle);
   printf("%s's age is now %i\n", myrtle.name, myrtle.age);
   return 0;
 }
