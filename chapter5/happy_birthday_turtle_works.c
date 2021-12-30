3abf622f5269fdea2f891d932c06eec41a8e9049
준규 <vpark45@gmail.com>
2021-12-30 19:04:20 +0900

    Modify from star and dot to arrow

--- chapter5/happy_birthday_turtle_works.c
+++ chapter5/happy_birthday_turtle_works.c
@@ -8,9 +8,9 @@ typedef struct {
 
 void happy_birthday(turtle *t)
 {
-  (*t).age = (*t).age + 1;
+  t->age = t->age + 1;
   printf("Happy Birthday %s! You are now %i years old!\n",
-    (*t).name, (*t).age);
+    t->name, t->age);
 }
 
 int main()
