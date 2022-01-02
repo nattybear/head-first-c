5f0cb34a2f9e5ba798484f6cf05c313ce7d9e857
준규 <vpark45@gmail.com>
2022-01-03 07:40:29 +0900

    Add test drive

--- chapter6/tour.c
+++ chapter6/tour.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
 #include "island.h"
 
 int main()
 {
-  island amity = {"Amity", "09:00", "17:00", NULL};
-  island craggy = {"Craggy", "09:00", "17:00", NULL};
-  island isla_nublar = {"Isla Nublar", "09:00", "17:00", NULL};
-  island shutter = {"Shutter", "09:00", "17:00", NULL};
-  amity.next = &craggy;
-  craggy.next = &isla_nublar;
-  island skull = {"Skull", "09:00", "17:00", NULL};
-  isla_nublar.next = &skull;
-  skull.next = &shutter;
-  display(&amity);
+  island *start = NULL;
+  island *i = NULL;
+  island *next = NULL;
+  char name[80];
+  for (; fgets(name, 80, stdin) != NULL; i = next) {
+    next = create(name);
+    if (start == NULL)
+      start = next;
+    if (i != NULL)
+      i->next = next;
+  }
+  display(start);
+  release(start);
   return 0;
 }
