fcbc735b478e877ea1406c5a62b383329e421ae2
준규 <vpark45@gmail.com>
2022-01-03 07:27:20 +0900

    Separate function create() to header

--- chapter6/island.h
+++ chapter6/island.h
@@ -11,3 +11,13 @@ void display(island *start)
   for (; i != NULL; i = i->next)
     printf("Name: %s open: %s-%s\n", i->name, i->opens, i->closes);
 }
+
+island* create(char *name)
+{
+  island *i = malloc(sizeof(island));
+  i->name = strdup(name);
+  i->opens = "09:00";
+  i->closes = "17:00";
+  i->next = NULL;
+  return i;
+}
--- chapter6/test_flight.c
+++ chapter6/test_flight.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "island.h"
-
-island* create(char *name)
-{
-  island *i = malloc(sizeof(island));
-  i->name = name;
-  i->opens = "09:00";
-  i->closes = "17:00";
-  i->next = NULL;
-  return i;
-}
 
 int main()
 {
