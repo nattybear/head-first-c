c52dff3cdeafe554a1ab1b640df118100b541050
준규 <vpark45@gmail.com>
2022-01-04 14:10:51 +0900

    Make function argument

--- chapter7/find.c
+++ chapter7/find.c
@@ -12,15 +12,34 @@ char *ADS[] = {
   "Jed: DBM likes theater, books and dining"
 };
 
-void find()
+int sports_no_bieber(char *s)
+{
+  return strstr(s, "sports") && !strstr(s, "bieber");
+}
+
+int sports_or_workout(char *s)
+{
+  return strstr(s, "sports") || strstr(s, "working out");
+}
+
+int ns_theater(char *s)
+{
+  return strstr(s, "NS") && strstr(s, "theater");
+}
+
+int arts_theater_or_dining(char *s)
+{
+  return strstr(s, "arts") || strstr(s, "theater") || strstr(s, "dining");
+}
+
+void find(int(*match)(char*))
 {
   int i;
   puts("Search results:");
   puts("------------------------------------");
 
   for (i = 0; i < NUM_ADS; i++)
-    if (strstr(ADS[i], "sports")
-        && !strstr(ADS[i], "bieber"))
+    if (match(ADS[i]))
       printf("%s\n", ADS[i]);
 
   puts("------------------------------------");
@@ -28,6 +47,9 @@ void find()
 
 int main()
 {
-  find();
+  find(sports_no_bieber);
+  find(sports_or_workout);
+  find(ns_theater);
+  find(arts_theater_or_dining);
   return 0;
 }
