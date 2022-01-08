0db09669c289400c9c293cc37bdcf30b99ba3257
준규 <vpark45@gmail.com>
2022-01-08 22:39:08 +0900

    Update newshound.c using fork()

--- chapter9/newshound.c
+++ chapter9/newshound.c
@@ -15,10 +15,17 @@ int main(int argc, char *argv[])
     char var[255];
     sprintf(var, "RSS_FEED=%s", feeds[i]);
     char *vars[] = {var, NULL};
-    if (execle("/usr/bin/python", "/usr/bin/python",
-               "./rssgossip.py", phrase, NULL, vars) == -1) {
-      fprintf(stderr, "Can't run script: %s\n", strerror(errno));
+    pid_t pid = fork();
+    if (pid == -1) {
+      fprintf(stderr, "Can't fork process: %s\n", strerror(errno));
       return 1;
+    }
+    if (!pid) {
+      if (execle("/usr/bin/python", "/usr/bin/python",
+                 "./rssgossip.py", phrase, NULL, vars) == -1) {
+        fprintf(stderr, "Can't run script: %s\n", strerror(errno));
+        return 1;
+      }
     }
   }
   return 0;
