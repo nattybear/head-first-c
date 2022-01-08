e4a3335e6a980858a0d79c9a9dcf8a4c712a8ef2
준규 <vpark45@gmail.com>
2022-01-08 22:27:47 +0900

    It runs only once

---
+++ chapter9/newshound.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+  char *feeds[] = {"http://www.cnn.com/rss/celebs.xml",
+                   "http://www.rollingstone.com/rock.xml",
+                   "http://eonline.com/gossip.xml"};
+  int times = 3;
+  char *phrase = argv[1];
+  int i;
+  for (i = 0; i < times; i++) {
+    char var[255];
+    sprintf(var, "RSS_FEED=%s", feeds[i]);
+    char *vars[] = {var, NULL};
+    if (execle("/usr/bin/python", "/usr/bin/python",
+               "./rssgossip.py", phrase, NULL, vars) == -1) {
+      fprintf(stderr, "Can't run script: %s\n", strerror(errno));
+      return 1;
+    }
+  }
+  return 0;
+}
