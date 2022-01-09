77c857d38acbc0ba54c3149c0304ce9bc1c68725
준규 <vpark45@gmail.com>
2022-01-09 19:51:10 +0900

    Create newshound2

---
+++ chapter10/newshound2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "error.h"
+
+int main(int argc, char *argv[])
+{
+  char *phrase = argv[1];
+  char *vars[] = {"RSS_FEED=http://rss.cnn.com/rss/edition.rss", NULL};
+  FILE *f = fopen("stories.txt", "w");
+  if (!f) {
+    error("Can't open stories.txt");
+  }
+  pid_t pid = fork();
+  if (pid == -1) {
+    error("Can't fork process");
+  }
+  if (!pid) {
+    if (dup2(fileno(f), 1) == -1) {
+      error("Can't redirect Standard Output");
+    }
+    if (execle("/usr/bin/python", "/usr/bin/python", "./rssgossip.py",
+          phrase, NULL, vars) == -1) {
+      error("Can't run script");
+    }
+  }
+  return 0;
+}
