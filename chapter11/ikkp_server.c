9e022df6bb76985d9e83739e92f5ac665be49072
준규 <vpark45@gmail.com>
2022-01-15 23:28:10 +0900

    Add fork()

--- chapter11/ikkp_server.c
+++ chapter11/ikkp_server.c
@@ -92,20 +92,28 @@ int main(int argc, char *argv[])
                            &address_size);
     if (connect_d == -1)
       error("Can't open secondary socket");
-    if (say(connect_d,
-            "Internet Knock-Knock Protocol Server\r\nVersion 1.0\r\nKnock! Knock!\r\n> ") != -1) {
-      read_in(connect_d, buf, sizeof(buf)); 
-      if (strncasecmp("Who's there?", buf, 12))
-        say(connect_d, "You should say 'Who's there?'!");
-      else {
-        if (say(connect_d, "Oscar\r\n> ") != -1) {
-          read_in(connect_d, buf, sizeof(buf));
-          if (strncasecmp("Oscar who?", buf, 10))
-            say(connect_d, "You should say 'Oscar who?'!\r\n");
-          else
-            say(connect_d, "Oscar silly question, you get a silly answer\r\n");
+
+    if (!fork()) {
+      close(listener_d);
+      if (say(connect_d,
+              "Internet Knock-Knock Protocol Server\r\nVersion 1.0\r\nKnock! Knock!\r\n> ")
+          != -1) {
+        read_in(connect_d, buf, sizeof(buf)); 
+
+        if (strncasecmp("Who's there?", buf, 12))
+          say(connect_d, "You should say 'Who's there?'!");
+        else {
+          if (say(connect_d, "Oscar\r\n> ") != -1) {
+            read_in(connect_d, buf, sizeof(buf));
+            if (strncasecmp("Oscar who?", buf, 10))
+              say(connect_d, "You should say 'Oscar who?'!\r\n");
+            else
+              say(connect_d, "Oscar silly question, you get a silly answer\r\n");
+          }
         }
       }
+      close(connect_d);
+      exit(0);
     }
     close(connect_d);
   }
